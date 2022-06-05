0d4a5bb2cacfdd425c9a3334d62c188bd2850b45
Richard Byrne <richardbyrne1995@gmail.com>
2022-06-05 17:16:45 +0100

    Fix db name bug, display blog posts on blog home from db

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
 import os
 from flask_pymongo import MongoClient
-from flask import Flask, flash, render_template
-if os.path.exists("env.py"):
-    import env
+from flask import Flask, flash, render_template, redirect, url_for
 
 
 app = Flask(__name__)
@@ -12,7 +10,7 @@ app.config["MONGO_URI"] = os.getenv("MONGO_URI")
 app.secret_key = os.getenv("SECRET_KEY")
 
 mongo = MongoClient(os.getenv("MONGO_URI"))
-db = mongo.get_database("ServerlessInstanceRBP")
+db = mongo.get_database("RBP")
 
 
 @app.route("/")
@@ -61,14 +59,33 @@ def contact():
 
 @app.route("/blog")
 def blog_home():
-    blog_posts = db.blog_posts
+    blog_posts = list(db.blog_posts.find())
     return render_template("blog_home.html",
                            page_wrapper_id="blog-wrapper",
                            blog_posts=blog_posts)
 
+
+@app.route("/insert_test_blog_post")
+def insert_test_blog_post():
+    test_blog_post = {
+        "title": "Test Blog Post",
+        "hero_image": "static/images/studioone/studio_one_3_amps.jpg",
+        "author": "Richard Byrne",
+        "url": "test-url"
+    }
+    try:
+        db.blog_posts.insert_one(test_blog_post)
+
     except Exception as e:
-        flash("Error loading page. ", f"Error: {e}")
-        return render_template("404.html")
+        flash(f"Error: {e}")
+
+    return redirect(url_for('home'))
+
+
+@app.route("/blog/<blog_url>")
+def blog_post(blog_url: str):
+    blog_object = db.blog_posts.find_one({"url": blog_url})
+    return render_template("blog_post_template.html", blog_post=blog_object)
 
 
 if __name__ == "__main__":
